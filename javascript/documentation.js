7eee775fc58f5a3f96152d7698043f5ec0d8d7b2
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-03-25 14:53:09 +0300

    mobile media  query

---
+++ javascript/documentation.js
@@ -0,0 +1,10 @@
+function openSlideNav() {
+    document.getElementById("navbar").style.width = "10rem";
+    // document.getElementById("main").style.marginLeft = "10rem";
+}
+
+// for closing the slide navigation bar
+function closeSlideNav() {
+    document.getElementById("navbar").style.width = "0";
+    // document.getElementById("main").style.marginLeft = "0";
+}
