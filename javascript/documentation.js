a798f5b0e023dd571eec320d3654bf3a4ef7d8a1
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-03-25 17:59:09 +0300

    minor fix

--- javascript/documentation.js
+++ javascript/documentation.js
@@ -1,10 +1,10 @@
 function openSlideNav() {
     document.getElementById("navbar").style.width = "10rem";
-    // document.getElementById("main").style.marginLeft = "10rem";
+    //  document.getElementById("main-doc").style.marginLeft = "5rem";
 }
 
 // for closing the slide navigation bar
 function closeSlideNav() {
     document.getElementById("navbar").style.width = "0";
-    // document.getElementById("main").style.marginLeft = "0";
+    // document.getElementById("main-doc").style.marginLeft = "0";
 }
